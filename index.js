f25ebe0351f1106124a5cfa942ad7b458f77ce7c
birdofartemis <72250724+birdofartemis@users.noreply.github.com>
2021-11-29 09:43:31 -0100

    creating a web server on port 3000

---
+++ index.js
@@ -0,0 +1,15 @@
+const exprees = require('express');
+const path = require('path');
+const app = exprees();
+
+app.use(exprees.static('./public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+const port = 3000;
+
+app.listen(port,  () => {
+    console.log(`Listenning on port: ${port}`);
+});
